8d2e2a2c4e91b7fef7059952295ec4e17bea4bd6
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-03-08 00:23:23 -0500

    feat: Add initial code for App.java

---
+++ src/main/java/edu/eci/arcn/bdd/App.java
@@ -0,0 +1,20 @@
+package edu.eci.arcn.bdd;
+
+/**
+ * Application entry point.
+ *
+ * @author Jesús Pinzón
+ * @version 1.0
+ * @since 2026-03-07
+ */
+public class App {
+
+    /**
+     * Main method.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
